4283942250b6a8d1f439b29400955e75efb66e0e
David Mally <djmally@gmail.com>
2015-01-08 16:49:36 -0500

    Removed thread junk

--- lakitu.cpp
+++ lakitu.cpp
@@ -21,54 +21,27 @@ using namespace std;
 using namespace mvIMPACT::acquire;
 
 
-//-----------------------------------------------------------------------------
-class ThreadParameter
-//-----------------------------------------------------------------------------
-{
-    Device*         m_pDev;
-    volatile bool   m_boTerminateThread;
-public:
-    ThreadParameter( Device* pDev ) : m_pDev( pDev ), m_boTerminateThread( false ) {}
-    Device* device( void ) const
-    {
-        return m_pDev;
-    }
-    bool    terminated( void ) const
-    {
-        return m_boTerminateThread;
-    }
-    void    terminateThread( void )
-    {
-        m_boTerminateThread = true;
-    }
-};
-
-//-----------------------------------------------------------------------------
-static unsigned int thread_func( void* pData )
-//-----------------------------------------------------------------------------
-{
-    ThreadParameter* pThreadParameter = reinterpret_cast<ThreadParameter*>( pData );
+static unsigned int capture_data(Device *pDev) {
     unsigned int cnt = 0;
 
-    try
-    {
-        pThreadParameter->device()->open();
-    }
-    catch( const ImpactAcquireException& e )
-    {
+    try {
+        pDev->open();
+    } catch(const ImpactAcquireException& e) {
         // this e.g. might happen if the same device is already opened in another process...
-        cout << "An error occurred while opening the device " << pThreadParameter->device()->serial.read()
+        //cout << "An error occurred while opening the device " << pThreadParameter->device()->serial.read()
+        cout << "An error occurred while opening the device " << pDev->serial.read()
              << "(error code: " << e.getErrorCode() << "(" << e.getErrorCodeAsString() << ")). Terminating thread." << endl
              << "Press [ENTER] to end the application..."
              << endl;
         getchar();
-        return 0;
+        return -1;
     }
 
     // establish access to the statistic properties
-    Statistics statistics( pThreadParameter->device() );
+    //Statistics statistics(pThreadParameter->device());
+    Statistics statistics(pDev);
     // create an interface to the device found
-    FunctionInterface fi( pThreadParameter->device() );
+    FunctionInterface fi(pDev);
 
     // prefill the capture queue. There can be more then 1 queue for some device, but for this sample
     // we will work with the default capture queue. If a device supports more then one capture or result
@@ -76,13 +49,11 @@ static unsigned int thread_func( void* pData )
     // queue only. Request as many images as possible. If there are no more free requests 'DEV_NO_FREE_REQUEST_AVAILABLE'
     // will be returned by the driver.
     int result = DMR_NO_ERROR;
-    SystemSettings ss( pThreadParameter->device() );
+    SystemSettings ss(pDev);
     const int REQUEST_COUNT = ss.requestCount.read();
-    for( int i = 0; i < REQUEST_COUNT; i++ )
-    {
+    for(int i = 0; i < REQUEST_COUNT; i++) {
         result = fi.imageRequestSingle();
-        if( result != DMR_NO_ERROR )
-        {
+        if(result != DMR_NO_ERROR) {
             cout << "Error while filling the request queue: " << ImpactAcquireException::getErrorCodeAsString( result ) << endl;
         }
     }
@@ -95,139 +66,68 @@ static unsigned int thread_func( void* pData )
     // we always have to keep at least 2 images as the display module might want to repaint the image, thus we
     // can't free it unless we have a assigned the display to a new buffer.
     int lastRequestNr = INVALID_ID;
-    while( !pThreadParameter->terminated() )
-    {
+    //while(!pThreadParameter->terminated()) {
+    while(true) {
         // wait for results from the default capture queue
-        requestNr = fi.imageRequestWaitFor( timeout_ms );
-        if( fi.isRequestNrValid( requestNr ) )
-        {
-            pRequest = fi.getRequest( requestNr );
-            if( pRequest->isOK() )
-            {
+        requestNr = fi.imageRequestWaitFor(timeout_ms);
+        if(fi.isRequestNrValid(requestNr)) {
+            pRequest = fi.getRequest(requestNr);
+            if(pRequest->isOK()) {
                 ++cnt;
                 // here we can display some statistical information every 100th image
-                if( cnt % 100 == 0 )
-                {
-                    cout << "Info from " << pThreadParameter->device()->serial.read()
+                if(cnt % 100 == 0) {
+                    //cout << "Info from " << pThreadParameter->device()->serial.read()
+                    cout << "Info from " << pDev->serial.read()
                          << ": " << statistics.framesPerSecond.name() << ": " << statistics.framesPerSecond.readS()
                          << ", " << statistics.errorCount.name() << ": " << statistics.errorCount.readS()
                          << ", " << statistics.captureTime_s.name() << ": " << statistics.captureTime_s.readS() << endl;
                 }
-            }
-            else
-            {
+            } else {
                 cout << "Error: " << pRequest->requestResult.readS() << endl;
             }
-            if( fi.isRequestNrValid( lastRequestNr ) )
-            {
+
+            if(fi.isRequestNrValid(lastRequestNr)) {
                 // this image has been displayed thus the buffer is no longer needed...
-                fi.imageRequestUnlock( lastRequestNr );
+                fi.imageRequestUnlock(lastRequestNr);
             }
             lastRequestNr = requestNr;
             // send a new image request into the capture queue
             fi.imageRequestSingle();
-        }
-        else
-        {
+        } else {
             // If the error code is -2119(DEV_WAIT_FOR_REQUEST_FAILED), the documentation will provide
             // additional information under TDMR_ERROR in the interface reference (
-            cout << "imageRequestWaitFor failed (" << requestNr << ", " << ImpactAcquireException::getErrorCodeAsString( requestNr ) << ", device " << pThreadParameter->device()->serial.read() << ")"
-                 << ", timeout value too small?" << endl;
+            cout << "imageRequestWaitFor failed (" << requestNr << ", " << ImpactAcquireException::getErrorCodeAsString( requestNr )
+                 << ", device " << pDev->serial.read() << ") timeout value too small?" << endl;
         }
     }
 
     // free the last potential locked request
-    if( fi.isRequestNrValid( requestNr ) )
-    {
-        fi.imageRequestUnlock( requestNr );
+    if(fi.isRequestNrValid(requestNr)) {
+        fi.imageRequestUnlock(requestNr);
     }
     // clear the request queue
-    fi.imageRequestReset( 0, 0 );
+    fi.imageRequestReset(0,0);
     return 0;
 }
 
-//-----------------------------------------------------------------------------
-static void* liveThread( void* pData )
-//-----------------------------------------------------------------------------
-{
-    thread_func( pData );
-    return NULL;
-}
-
-//-----------------------------------------------------------------------------
-int main( int /*argc*/, char* /*argv*/[] )
-//-----------------------------------------------------------------------------
-{
+int main( int /*argc*/, char* /*argv*/[] ) {
     DeviceManager devMgr;
     const unsigned int devCnt = devMgr.deviceCount();
-    if( devCnt == 0 )
-    {
+    if( devCnt == 0 ) {
         cout << "No MATRIX VISION device found! Unable to continue!" << endl;
         return 0;
     }
-    //pthread_t* pHandle = new pthread_t[devCnt];
-    //pthread_attr_t* pAttr = new pthread_attr_t[devCnt];
-    pthread_t *pHandle = new pthread_t();
-    pthread_attr_t *pAttr = new pthread_attr_t();
-    // store all device infos in a vector
-    // and start the execution of a 'live' thread for each device.
-    //vector<ThreadParameter*> threadParams;
     Device *pDev = getDeviceFromUserInput(devMgr);
-    //ThreadParameter *threadParam = new ThreadParameter(devMgr[devIdx]);
-    ThreadParameter *threadParam = new ThreadParameter(pDev);
-    /*for( unsigned int i = 0; i < devCnt; i++ )
-    {
-        threadParams.push_back( new ThreadParameter( devMgr[i] ) );
-        cout << devMgr[i]->family.read() << "(" << devMgr[i]->serial.read() << ")" << endl;
-    }*/
 
-
-    // start live threads
-    /*for( unsigned int j = 0; j < devCnt; j++ )
-    {
-        pthread_attr_init( &pAttr[j] );
-        // you can set the stack size like this: pthread_attr_setstacksize (&pAttr[j], 1024*1024);
-        pthread_create( &pHandle[j], &pAttr[j], liveThread, ( void* )threadParams[j] );
-    }*/
-    pthread_attr_init(pAttr);
-    pthread_create(pHandle, pAttr, liveThread, (void *)threadParam);
+    // start capture
+    capture_data(pDev);
 
     // now all threads will start running...
     cout << "Press any key to end the acquisition" << endl;
     getchar();
 
     // stop all threads again
-    cout << "Terminating live threads..." << endl;
-    /*size_t vSize = threadParams.size();
-    for( unsigned int k = 0; k < vSize; k++ )
-    {
-        cout << "Terminating thread " << k << "." << endl;
-        threadParams[k]->terminateThread();
-    }*/
-    threadParam->terminateThread();
+    cout << "Terminating capture..." << endl;
 
-    // wait until each live thread has terminated.
-    /*for( unsigned int j = 0; j < devCnt; j++ )
-    {
-        cout << "Waiting for thread " << j << " to terminate." << endl;
-        pthread_join ( pHandle[j], NULL );
-    }*/
-    pthread_join(*pHandle, NULL);
-    cout << "All capture threads terminated." << endl;
-
-    // free resources
-    /*for( unsigned int l = 0; l < vSize; l++ )
-    {
-        delete threadParams[l];
-        cout << "thread parameter " << l << " removed." << endl;
-    }
-    delete [] pHandle;*/
-    delete threadParam;
-    delete pHandle;
-    cout << "All thread handles removed." << endl;
-
-    //delete [] pAttr;
-    delete pAttr;
-    cout << "All thread atributes removed." << endl;
     return 0;
 }
